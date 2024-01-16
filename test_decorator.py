bbb07eb8999a35ebb30234835e27c4b9f7d429a7
matygullinruslan <matygullinruslan@gmail.com>
2024-01-16 22:25:55 +0300

    for checks

--- test_decorator.py
+++ test_decorator.py
@@ -1,8 +1,7 @@
 import allure
 from allure_commons.types import Severity
-from selene.support.conditions import be
 from selene.support.shared.jquery_style import s
-from selene import browser, by
+from selene import browser, by, be
 
 
 @allure.step('Открываем главную страницу')
@@ -13,22 +12,22 @@ def open_main_page():
 @allure.step('Находим репозиторий {repo}')
 def search_for_repository(repo):
     s('.header-search-button').click()
-    s('#query-builder-test').type('matygullinruslan/qa_guru_python_9.7').press_enter()
+    s('#query-builder-test').type('repo').press_enter()
 
 
 @allure.step('Переходим по ссылке репозитория {repo}')
 def go_to_repository(repo):
-    s(by.link_text('matygullinruslan/qa_guru_python_9.7')).click()
+    s(by.link_text('repo')).click()
 
 
-@allure.step('Открываем таб Issues')
+@ allure.step('Открываем таб Issues')
 def open_issue_tab():
     s('#issues-tab').click()
 
 
 @allure.step('Проверяем наличие Issues с номером 1{number}')
 def should_see_issue_with_number(number):
-    s(by.partial_text('#1')).should(be.visible)
+    s(by.partial_text('number')).should(be.visible)
 
 
 def test_dynamic_labels():
@@ -43,16 +42,14 @@ def test_dynamic_labels():
 @allure.tag('web')
 @allure.label('owner', 'matygullin')
 @allure.link('https://github.com', name='Testing')
+
+
 def test_decorator_steps():
-    pass
-
-
-
-# open_main_page()
-# search_for_repository('matygullinruslan/qa_guru_python_9.7')
-# go_to_repository('matygullinruslan/qa_guru_python_9.7')
-# open_issue_tab()
-# should_see_issue_with_number('1')
+    open_main_page()
+    search_for_repository('matygullinruslan/qa_guru_python_9.7')
+    go_to_repository('matygullinruslan/qa_guru_python_9.7')
+    open_issue_tab()
+    should_see_issue_with_number('#1')
 
 
 # @allure.feature('Задачи в репозитории')
